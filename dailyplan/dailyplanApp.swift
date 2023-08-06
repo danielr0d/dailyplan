e4d6ed0d6ec234e45553da4b3743f37babd64d5f
Daniel Rodrigues <daniel.rodrigues@yazo.com.br>
2023-08-06 15:55:56 -0300

    initial commit

---
+++ dailyplan/dailyplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyplanApp.swift
+//  dailyplan
+//
+//  Created by Daniel Rodrigues on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct dailyplanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
