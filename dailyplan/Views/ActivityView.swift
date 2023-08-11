560fbc6877cf7afd96cd36cbf83652042485ba04
Daniel Rodrigues <daniel.rodrigues.98@hotmail.com>
2023-08-10 23:08:25 -0300

    add activityview

---
+++ dailyplan/Views/ActivityView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityView.swift
+//  dailyplan
+//
+//  Created by Daniel Rodrigues on 10/08/23.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text("Acitivity List")
+    }
+}
+
+struct ActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityView()
+    }
+}
